fb90f65246b45de29bdd87bd1bc38685e1b7f883
varshneyshubham <42005954+varshneyshubham@users.noreply.github.com>
2018-08-09 23:45:43 +0530

    day-2-100-days-of-coding

---
+++ day-2/area of rectangle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int length,breadth,area;
+printf("enter a length and breadth of an rectangle:\n");
+scanf("%d%d",&length,&breadth);
+area=length*breadth;
+printf("area of an rectangle is%d ",area);
+getch();
+}
