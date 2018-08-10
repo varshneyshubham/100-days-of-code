0669c02617f6a5317d41f7a8a8cb4ee8ae45cc6f
varshneyshubham <42005954+varshneyshubham@users.noreply.github.com>
2018-08-10 23:36:29 +0530

    day-3-100-days-of-coding

---
+++ day-3/ASCII.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+    char c;
+    clrscr();
+    printf("Enter a character: ");
+
+  
+    scanf("%c", &c);  
+    
+  
+    printf("ASCII value of %c = %d", c, c);
+    getch();
+}
