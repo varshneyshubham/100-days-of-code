5c408dc6fbfd3ff721e6c51cce80e1fc08e0691b
varshneyshubham <42005954+varshneyshubham@users.noreply.github.com>
2018-08-12 22:15:41 +0530

    day-5-100-days-of-coding

---
+++ day-5/+ve or -ve.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<conio.h>
+ 
+void main()
+{
+    int num;
+    clrscr();
+ 
+    printf("Enter a number: \n");
+    scanf("%d", &num);
+    if (num > 0)
+        printf("%d is a positive number \n", num);
+    else if (num < 0)
+        printf("%d is a negative number \n", num);
+    else
+        printf("0 is neither positive nor negative");
+        getch();
+}
