1e4a7936450c9638f7f31334675dde424242ccdc
varshneyshubham <42005954+varshneyshubham@users.noreply.github.com>
2018-08-16 23:03:56 +0530

    day-8-100-days-of-coding

---
+++ day-8/simple calculator.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+void main()
+{
+
+    char operator;
+    double num1,num2;
+
+    printf("Enter an operator (+, -, *,/): ");
+    scanf("%c", &operator);
+
+    printf("Enter two operands: ");
+    scanf("%lf %lf",&num1, &num2);
+
+    switch(operator)
+    {
+        case '+':
+            printf("%.1lf + %.1lf = %.1lf",num1, num2, num1+ num2);
+            break;
+
+        case '-':
+            printf("%.1lf - %.1lf = %.1lf",num1, num2, num1 - num2);
+            break;
+
+        case '*':
+            printf("%.1lf * %.1lf = %.1lf",num1, num2, num1 * num2);
+            break;
+
+        case '/':
+            printf("%.1lf / %.1lf = %.1lf",num1, num2, num1 / num2);
+            break;
+        default:
+            printf("Error! operator is not correct");
+    }
+    
+    getch();
+}
